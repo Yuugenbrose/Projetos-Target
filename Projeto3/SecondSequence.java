e2aa0b50ad33d7e37b3493b179529537a92d88e4
Yuugenbrose <149912977+Yuugenbrose@users.noreply.github.com>
2024-03-15 14:54:14 -0300

    Add files via upload

---
+++ Projeto3/SecondSequence.java
@@ -0,0 +1,10 @@
+public class SecondSequence {
+    public static void main(String[]args) {
+        
+        int number = 2;
+        for (int i = 0; i < 6; i++) { // A sequência parte do 2 e segue em multiplicação por 2, bastando multiplicar e usar o for para a repetição.
+            System.out.print(number + " ");
+            number *= 2;
+        }
+    }
+}
