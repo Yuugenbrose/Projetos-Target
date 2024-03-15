ee1b49977ec57b9fb1d2bd8f2aa95c0da0ca36ef
Yuugenbrose <149912977+Yuugenbrose@users.noreply.github.com>
2024-03-15 14:47:45 -0300

    Add files via upload

---
+++ Projeto3/FirstSequence.java
@@ -0,0 +1,7 @@
+public class FirstSequence {
+    public static void main(String[]args) {
+        for (int i = 1; i <= 7; i += 2) { // A sequência é uma sequência simples de números impares, bastando partir de um número impar e acrescentar 2 em loop for.
+            System.out.print(i + " ");
+        }
+    }
+}
